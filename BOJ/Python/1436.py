c40eff0a7d22e66b9e053e453a0ab77e683206c1
Kim Nahyeong <of.kimna@gmail.com>
2021-04-02 13:02:50 +0900

    Coding Study Week 4, Silver V

---
+++ BOJ/Python/1436.py
@@ -0,0 +1,16 @@
+import sys
+
+n = int(input())
+cnt = 0
+i = 0
+while(1):
+    if("666" in str(i)):
+        cnt += 1
+
+    if(n == cnt):
+        print(i)
+        break
+    i += 1
+
+#666 1666 2666 3666 4666 5666 6660~6669 7666 8666 9666 10666 16660~16669
+
