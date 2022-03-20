a804c9263babc6a1b57e69c843a27303a9b86988
Kim Nahyeong <of.kimna@gmail.com>
2022-03-20 20:02:50 +0900

    220319

---
+++ C++/5339.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int argc, char** argv){
+  printf("     /~\\\n");
+  printf("    ( oo|\n");
+  printf("    _\\=/_\n");
+  printf("   /  _  \\\n");
+  printf("  //|/.\\|\\\\\n");
+  printf(" ||  \\ /  ||\n");
+  printf("============\n");
+  printf("|          |\n");
+  printf("|          |\n");
+  printf("|          |\n");
+}
